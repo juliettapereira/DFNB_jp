eaa9a7a05562398072dd8d0e2d94351bbd4333c6
juliettapereira <juliettapereira@hotmail.com>
2020-06-15 18:05:49 -0600

    etl sync

---
+++ other_scripts/SQLServerCodeHeader.sql
@@ -0,0 +1,20 @@
+/*****************************************************************************************************************
+NAME:    dfnb.presentation.views
+PURPOSE: Create the dfnb.presentation.views
+
+SUPPORT: Julietta Pereira
+         juliettapereirao@gmail.com
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+1.0       6/6/2020		JPereira 	1. Built 4 views
+
+
+RUNTIME: 
+1 min
+
+NOTES: 
+These views are used as the information source for the DFNB presentation 1.
+
+******************************************************************************************************************/
